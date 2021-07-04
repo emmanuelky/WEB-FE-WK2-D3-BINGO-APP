0613e4b7ab1a92bfec04c4c380dcd01ea3933210
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-07-04 10:32:28 +0200

    Completed

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
-window.onload = function () {
-    displayBoardNumbers()
-}
-
-
-
-const displayBoardNumbers = function () {
-
-    let boardContainerNode = document.getElementById("board-container")
-
-    for (let num = 1; num <= 76; num++) {
-
-        let newBoardNode = document.createElement("div")
-        newBoardNode.innerText = num
-        newBoardNode.classList.add("board")
-
-        boardContainerNode.appendChild(newBoardNode)
+const generateMainBoard = function () {
+    const board = document.querySelector(".main-board");
+    for (let i = 0; i < 76; i++) {
+        board.innerHTML += `<div class='cell'>${i + 1}</div>`;
     }
-}
+};
 
-const randomNumber = function () {
-    let randomButton = document.querySelector("random-button")
-    alert("hello")
-}
+//create an arr in range [1,...76]
+const fillArray = function () {
+    const arr = [];
+    for (let i = 0; i < 76; i++) {
+        arr.push(i + 1);
+    }
+    return arr;
+};
+//generate rand index in the range of the arr.length
+//get a element in the arr under the index
+//remove the elemnt from the arr
+const getRandomNum = function (range) {
+    const randIndex = Math.floor(Math.random() * range.length);
+    const random = range.splice(randIndex, 1)[0];
+    return random;
+};
+
+const generateRandNumber = function (range) {
+    const random = getRandomNum(range);
+    const randNumDiv = document.getElementById("randNum");
+    randNumDiv.innerText = "Number: " + random;
+    const cells = document.querySelectorAll(".main-board .cell");
+    cells[random - 1].classList.add("highlight");
+
+    const userCells = document.querySelectorAll(".user-board .cell");
+    userCells.forEach((cell) => {
+        if (parseInt(cell.innerText) === random) {
+            cell.classList.add("highlight");
+        }
+    });
+};
+
+const generateUserBoards = function () {
+    const usersNumber = document.getElementById("usersNumber").value;
+    const container = document.querySelector(".container");
+    if (parseInt(usersNumber) > 0) {
+        for (let i = 0; i < parseInt(usersNumber); i++) {
+            const range = fillArray();
+            const board = document.createElement("div");
+            board.className = "board user-board";
+            for (let i = 0; i < 24; i++) {
+                const random = getRandomNum(range);
+                board.innerHTML += `<div class='cell'>${random}</div>`;
+            }
+            container.appendChild(board);
+        }
+    }
+};
+
+window.onload = function () {
+    generateMainBoard();
+    const randBtn = document.getElementById("randBtn");
+    const range = fillArray();
+    randBtn.addEventListener("click", function () {
+        generateRandNumber(range);
+    });
+    const userBoardBtn = document.getElementById("userBoardBtn");
+    userBoardBtn.onclick = generateUserBoards;
+};
